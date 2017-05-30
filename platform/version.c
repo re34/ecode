175709632d7261efdb4a6711dd596e634c2312d6
ArnoYuan <1614903252@qq.com>
2017-05-30 08:48:53 +0800

    1、发布版本Release v1.1.0

--- platform/version.c
+++ platform/version.c
@@ -4,7 +4,7 @@
 #define _STR(s) #s
 
 #define FW_VERSION_MAJOR    1
-#define FW_VERSION_MINOR    0
+#define FW_VERSION_MINOR    1
 #define FW_VERSION_BUILD    0
 
 #define MACRO_TO_STR(macro)     _STR(macro)
