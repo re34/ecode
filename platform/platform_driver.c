4f1622ee462a2ba1a32cfb847b87e042946fcf11
arno <1614903252@qq.com>
2017-06-07 13:34:42 +0800

    1、改写platform_driver，使用静态数组装在设备

--- platform/platform_driver.c
+++ platform/platform_driver.c
@@ -4,52 +4,130 @@
 
 LIST_HEAD(platform_device_head);
 
+static struct platform_device * devs[DEVICEn]={NULL};
 
-int platform_driver_register(struct platform_device *dev)
+
+int platform_driver_register(device_name_t name,struct platform_device *dev)
 {
     if(dev==NULL)
         return -1;
     
+    if(devs[name]!=NULL)
+        return -2;
+    
+    devs[name] = dev;
     list_add(&platform_device_head, &dev->entry);
+    
+    LOG_DEBUG("%s is registered!", dev->name);
     
     return 0;
 }
 
-int platform_driver_unregister(struct platform_device *dev)
+int platform_driver_unregister(device_name_t name)
 {
-    list_del(&dev->entry);
+
+    if(devs[name]!=NULL)
+    {
+        list_del(&devs[name]->entry);
+        LOG_DEBUG("%s is unregistered!", devs[name]->name);
+    }
+    devs[name]=NULL;
+    
     return 0;
 }
 
 int open(const char *name, int flags)
 {
-    struct list_head *dev;
-    struct list_head *tnode = NULL;
+    int i;
+    int ret;
+    struct platform_device *dev = NULL;
+    struct device_operations *ops = NULL;
     
-    list_for_each_safe(dev, tnode, &platform_device_head)
+    for(i=0;i<DEVICEn;i++)
     {
-        if(0!=strcmp(((struct platform_device *)dev)->name, name))
+        dev = devs[i];
+        if(dev==NULL)
             continue;
-        LOG_DEBUG("%s is opned!");
-        return (int)dev;
+        if(0==strcmp(name, dev->name))
+        {
+            if(dev->ops!=NULL)
+            {
+                ops = dev->ops;
+                if(ops->open==NULL)
+                {
+                    LOG_ERROR("%s has no operation named open!", name);
+                    return -1;
+                }
+                ret = ops->open(dev, flags);
+                if(ret<0)
+                    return -2;
+                else
+                    return i;
+            }
+            else
+            {
+                LOG_ERROR("%s has no operation!", name);
+                return -3;
+            }
+        }
     }
     
-    return -1;
+    LOG_ERROR("%s can not found!", name);
+    
+    return -4;
 }
 
 int close(int fd)
 {
+    int i;
+    int ret;
+    struct platform_device *dev = NULL;
+    struct device_operations *ops = NULL;
     
-    return 0;
+    for(i=0;i<DEVICEn;i++)
+    {
+        dev = devs[i];
+        if(dev==NULL)
+            continue;
+
+            if(dev->ops!=NULL)
+            {
+                ops = dev->ops;
+                if(ops->close==NULL)
+                {
+                    LOG_ERROR("%d has no operation named close!", fd);
+                    return -1;
+                }
+                ret = ops->close(dev);
+                if(ret<0)
+                    return -2;
+                else
+                    return i;
+            }
+            else
+            {
+                LOG_ERROR("%d has no operation!", fd);
+                return -3;
+            }
+        
+    }
+    
+    LOG_ERROR("%d can not found!", fd);
+    
+    return -4;
 }
 
 int write(int fd, char *datas, int len)
 {
     struct device_operations *ops;
-    struct platform_device *dev = (struct platform_device *)fd;
+    struct platform_device *dev;  
     char *pdata = datas;
+    
     int ret = 0;
     int temp = 0;
+    
+    
+    dev = devs[fd];
     
     ops = dev->ops;
     
@@ -69,10 +147,12 @@ int write(int fd, char *datas, int len)
 int read(int fd, char *buf, int len)
 {
     struct device_operations *ops;
-    struct platform_device *dev = (struct platform_device *)fd;
+    struct platform_device *dev;
     char *pbuf = buf;
     int ret = 0;
     int temp = 0;
+    
+    dev = devs[fd];
     
     ops = dev->ops;
     
@@ -92,7 +172,10 @@ int read(int fd, char *buf, int len)
 int ioctl(int fd, int cmd, int arg)
 {
     struct device_operations *ops;
-    struct platform_device *dev = (struct platform_device *)fd;
+    struct platform_device *dev;
+    
+    
+    dev = devs[fd];
     
     ops = dev->ops;
     
