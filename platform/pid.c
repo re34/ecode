29a20e0cf2d65589c3720b84f479bc1c2c71dfed
ArnoYuan <1614903252@qq.com>
2017-06-18 19:27:57 +0800

    1、修正pid控制算法，实现位置式和增量式pid算法

--- platform/pid.c
+++ platform/pid.c
@@ -1,10 +1,23 @@
+/*
+u(t)=Kp[e(k)+T/Ti∑e(j)+Td/T[e(k)−e(k−1)]]
+    =Kp*e(k)+Kp*T/Ti∑e(j)+Kp*Td/T[e(k)-e(k-1)]
+    =Kp*e(k)+Ki*∑e(j)+Kd*[e(k)-e(k-1)]
+Ki = Kp*T/Ti
+Kd = Kp*Td/T
+
+Δu(k)=u(k)−u(k−1)
+     =Kp[e(k)-e(k-1)]+Ki*e(k)+kd[e(k)-2e(k-1)+e(k-2)]
+*/
+
 #include "pid.h"
 #include "includes.h"
 
+
+
+
 int pid_init(struct pid *pid)
 {
-    if(pid==NULL)
-        return -1;
+    ASSERT_PARAM(pid!=NULL);
     
     pid->set_val = 0;
     pid->err = 0;
@@ -17,25 +30,75 @@ int pid_init(struct pid *pid)
     return 0;
 }
 
-
-int pid_increment_calc(struct pid *pid, float feadback)
+void pid_set_desired(struct pid *pid, float val)
 {
-    float derr;
+    ASSERT_PARAM(pid!=NULL);
+    
+    pid->set_val = val;
+}
+
+void pid_set_threshold(struct pid *pid, float max, float min)
+{
+    ASSERT_PARAM(pid!=NULL);
+    
+    pid->output_max = max;
+    pid->output_min = min;
+}
+
+void pid_set_param(struct pid *pid, float kp, float ki, float kd)
+{
+    ASSERT_PARAM(pid!=NULL);
+    
+    pid->kp = kp;
+    pid->ki = ki;
+    pid->kd = kd;
+}
+
+
+float pid_position_calc(struct pid *pid, float feadback)
+{
     ASSERT_PARAM(pid!=NULL);
     
     pid->err = pid->set_val - feadback;
-    derr = pid->kp*(pid->err-pid->err_last)+pid->ki*pid->err\
-        +pid->kd*(pid->err-2*pid->err_last+pid->err_last_last);
+    pid->err_sum += pid->err;
+    
+    pid->output = pid->kp*pid->err+pid->ki*pid->err_sum+\
+                    pid->kd*(pid->err-pid->err_last);
 
+    pid->err_last = pid->err;
+    
+    if(pid->output>pid->output_max)
+        pid->output = pid->output_max;
+    if(pid->output<pid->output_min)
+        pid->output = pid->output_min;
+    
+    return pid->output;
+}
+
+float pid_increment_calc(struct pid *pid, float feedback)
+{
+    float duk = 0;
+    ASSERT_PARAM(pid!=NULL);
+    
+    pid->err = pid->set_val-feedback;
+    pid->err_sum += err;
+    
+    duk = pid->kp*(pid->err->pid->err_last)+pid->ki*pid->err+\
+          pid->kd*(pid->err-2*pid->err_last+pid->err_last_last);
+          
     pid->err_last_last = pid->err_last;
     pid->err_last = pid->err;
     
-    if(pid->output_val>pid->max)
-        pid->output_val = pid->max;
+    pid->output += duk;
     
-    if(pid->output_val<pid->min)
-        pid->output_val = pid->min;
+    if(pid->output>pid->output_max)
+        pid->output = pid->output_max;
+    if(pid->output<pid->output_min)
+        pid->output = pid->output_min;
     
-    return derr;
+    return pid->output;    
 }
+
+
+
 
--- platform/pid.h
+++ platform/pid.h
@@ -7,12 +7,18 @@ struct pid{
     float err;//偏差
     float err_last;
     float err_last_last;
+    float err_sum;
     float kp,ki,kd;
-    float max;
-    float min;
+    float output;
+    float output_max;
+    float output_min;
 };
 
 
 int pid_init(struct pid *pid);
-int pid_process(struct pid *pid);
+void pid_set_desired(struct pid *pid, float val);
+void pid_set_threshold(struct pid *pid, float max, float min);
+void pid_set_param(struct pid *pid, float kp, float ki, float kd);
+float pid_position_calc(struct pid *pid, float feadback);
+float pid_increment_calc(struct pid *pid, float feedback);
 #endif
